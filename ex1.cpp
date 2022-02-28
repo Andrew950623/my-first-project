7a999f78b0828494991fe71145fc0fbe7cd57ef3
Andrew950623 <100544152+Andrew950623@users.noreply.github.com>
2022-02-28 11:44:14 +0800

    Create ex1.cpp

---
+++ ex1.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+/*
+     I am 18 years old 
+     my height is 1 m 80 cm 
+     my weight is 70 kg 
+     I have 1 brother , and 1 sister 
+    */
+    printf("I am %d year old \n  ",18);
+    printf("my height is %d m %d cm \n ",1,80);
+    printf("my weight is %d kg \n ",70);
+    printf("I have %d brother, and %d sister \n ",1,1);
+    cout <<  "hello_Selena ";
+    cout <<  "my height is " << 1 << "m " << 80 << "cm";
+
+    return 0;
+}
