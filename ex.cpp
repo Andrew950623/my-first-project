ee7b941f6bccc9275e576c7429e1eb81e9a4b811
Andrew950623 <100544152+Andrew950623@users.noreply.github.com>
2022-03-03 19:15:24 +0800

    Create ex.cpp

---
+++ ex.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int myage = 16;
+    printf("Hello World ");
+    printf("I am %d \n ",myage);
+    
+    cout <<  "hello selena ";
+    cout <<  "my height is " << 1 << "m " << 80 << "cm";
+
+    return 0;
+}
